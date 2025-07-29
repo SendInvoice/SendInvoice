b0b17848f8e5505699d26d9989e05e03dbbfccdc
Jorge Alvarez <145654953+Jorgealvarezpb7@users.noreply.github.com>
2025-07-29 18:17:03 +0200

    configura rutas (#13)
    * adding routes
    
    * todos los caminos conducen a Roma

--- pkgs/client/src/App.tsx
+++ pkgs/client/src/App.tsx
@@ -1,12 +1,21 @@
+import { StrictMode } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import Auth from './pages/Auth';
+import Dashboard from './pages/Dashboard';
 
 import './App.css';
 
 function App() {
   return (
-    <>
-      <Auth />
-    </>
+    <StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </StrictMode>
   )
 }
 
